6921fe2176614eccfba5bbf42bc00d56a3e5e702
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-13 23:31:38 +0100

    i++-add

--- while/script.js
+++ while/script.js
@@ -1,3 +1,21 @@
 // Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
 // Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+// creo la lista
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+
+// creo una variabile let con valore 0
+let i = 0;
+//uso while per avere un ciclo
+
+while (i < list.length) {
+    console.log(list[i]);
+    i++;
+}
 
