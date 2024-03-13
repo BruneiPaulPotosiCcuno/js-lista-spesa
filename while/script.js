633527553583ffefbeb3d5fcf14c776a6a7b8cc7
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-13 23:23:23 +0100

    add-list

---
+++ while/script.js
@@ -0,0 +1,3 @@
+// Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+// Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+
